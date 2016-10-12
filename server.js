a0d7a1af9b00bcb0022b39e372f5011d338e0fe5
jacob bleser <jacob bleser>
2016-10-12 17:13:18 -0400

    Added monogodb connection

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
+var mongo = require('mongodb').MongoClient;
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res){
-	res.send("Atom Build Online!");
-});
 
-app.listen(process.env.PORT || 3000);
+
+mongo.connect('mongodb://localhost:27017/atom-test', function(err, db){
+	if (err) throw new Error('Database connection failure!');
+	else {
+		console.log("Database connection successful!");
+	}
+
+	app.get('/', function(req, res){
+		res.send("Atom Build Online and Database Connected!");
+	});
+
+	app.listen(process.env.PORT || 3000);
+});
