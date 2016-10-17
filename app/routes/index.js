f21045e193066623e63366144093399fb2595c58
jacob bleser <jacob bleser>
2016-10-17 12:24:29 -0400

    modified for clementine tutorial

---
+++ app/routes/index.js
@@ -0,0 +1,15 @@
+var ClickHandler = require(process.cwd() + '/app/controllers/clickHandler.server.js');
+
+module.exports = function(app, db){
+	var clickHandler = new ClickHandler(db);
+
+	app.route('/')
+		.get(function(req, res){
+			res.sendFile(process.cwd() + '/public/html/index.html');
+		});
+
+	app.route('/api/clicks')
+		.get(clickHandler.getClicks)
+		.post(clickHandler.addClick)
+		.delete(clickHandler.resetClicks);
+};
--- server.js
+++ server.js
@@ -1,18 +1,20 @@
-var mongo = require('mongodb').MongoClient;
-var express = require('express');
+var express = require('express'),
+	 routes = require('./app/routes/index.js'),
+	 mongo = require('mongodb').MongoClient;
+
 var app = express();
 
-
-
 mongo.connect('mongodb://localhost:27017/atom-test', function(err, db){
-	if (err) throw new Error('Database connection failure!');
+	if (err) throw new Error("Failed to connect to database");
 	else {
-		console.log("Database connection successful!");
+		console.log("MongoDB successfully connected to port 27017");
 	}
 
-	app.get('/', function(req, res){
-		res.send("Atom Build Online and Database Connected!");
-	});
+	app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 
-	app.listen(process.env.PORT || 3000);
+	routes(app, db);
+
+	app.listen(3000, function(){
+		console.log("Listening on port 3000...");
+	});
 });
